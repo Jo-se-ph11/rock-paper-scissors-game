cc82f1f2efbe7ecfdd2d183306c1a160958d0742
Joseph <thejosephsamuel@gmail.com>
2022-09-02 16:04:00 +0100

    Add buttons and divs to main.js file to display result

--- main.js
+++ main.js
@@ -1,65 +1,94 @@
 //This function below generates a random word from the array
 
-let get_computer_choice = () => {
+function get_computer_choice(){
     let choice = ['paper', "scissors", "rock"];
-    let random_choice = choice[Math.floor(Math.random() * choice.length)];
+    let random_choice = choice[Math.floor(Math.random() * 3)];
     return random_choice;
 }
 
-let player_choice = prompt("Type rock, paper, or scissors");
+const btn = document.querySelectorAll("#btn");
+let player_score = 0;
+let computer_score = 0; 
 
-function play_round  ( player_choice, computer_choice) {
-   switch(true) {
-    case ( player_choice == "rock" && computer_choice =="scissors" ):
-        return "You Win! rock beats scissors";
-        return player_choice++;
-        break;
-    case ( player_choice == "scissors" && computer_choice ==  "rock" ):
-        return "Computer Win! rock beats scissors";
-        return computer_choice++;
-        break;
-    case ( player_choice == "paper" && computer_choice == "scissors" ):
-        return "Computer Win! scissors beats paper";
-        return computer_choice++;
-        break; 
-    case ( player_choice == "scissors" && computer_choice == "paper" ):
-        return "You win! scissors beats paper";
-        return player_choice++;
-        break;
-    case (player_choice == "rock" && computer_choice == "paper" ):
-        return "Computer Win! paper beats rock";
-        return computer_choice++;
-        break;
-    case ( player_choice == "paper" && computer_choice == "rock" ):
-        return "You Win! paper beats rock";
-        return player_choice++;
-        break;
-    case ( player_choice == "rock" && computer_choice == "rock" ):
-        return "It's a tie! rock and rock";
-        break;
-    case ( player_choice == "scissors" && computer_choice == "scissors" ):
-        return "it's a tie! scissors and scissors";
-        break;
-    case ( player_choice == "paper" && computer_choice == "paper" ):
-        return "It's a tie! paper and paper";
-        break;
-    default:
-        return "Invalid Input";
+btn.forEach((btn) => {
+
+    btn.addEventListener("click", function() {
+   
+       let player_choice = this.textContent;
+       play_round( player_choice, get_computer_choice() );
+       update_score();
+       if (check_winner()) {
+           player_score = computer_score = 0;
+           update_score();
+         }
+   
+   
+    })   
+   })
+
+   const result = document.createElement('div');
+   container.append(result);
+   
+   function play_round  ( player_choice, computer_choice) {
+    switch(true) {
+     case  ( player_choice == "rock" && computer_choice == "rock" ):
+        result.textContent = "It is a tie!";
+         return;
+         break;
+     case  ( player_choice == "scissors" && computer_choice =="scissors" ):
+        result.textContent = "it is a tie!";
+         return;
+         break;
+     case  ( player_choice == "paper" && computer_choice =="paper" ):
+        result.textContent = "It is a tie!";
+         return;
+         break;
+     case ( player_choice == "rock" && computer_choice =="scissors" ):
+        result.textContent = "You Win! rock beats scissors";
+         player_score++;
+         break;
+     case ( player_choice == "scissors" && computer_choice ==  "rock" ):
+          result.textContent = "Computer Win! rock beats scissors";
+         computer_score++;
+         break;
+     case ( player_choice == "paper" && computer_choice == "scissors" ):
+         result.textContent = "Computer Win! scissors beats paper";
+         computer_score++;
+         break; 
+     case ( player_choice == "scissors" && computer_choice == "paper" ):
+        result.textContent ="You win! scissors beats paper";
+         player_score++;
+         break;
+     case (player_choice == "rock" && computer_choice == "paper" ):
+        result.textContent = "Computer Win! paper beats rock";
+         computer_score++;
+         break;
+     case ( player_choice == "paper" && computer_choice == "rock" ):
+        result.textContent= "You Win! paper beats rock";
+         player_score++;
+         break;
+     default:
+         return "Invalid Input";
+    } 
    } 
+
+function update_score() {
+    document.getElementById("player-score").textContent = player_score;
+    document.getElementById("computer-score").textContent = computer_score;
 }
 
-const game = () => {
-    let player_choice = "";
-    for ( let i = 0; i < 5; i++) {
-        player_choice = prompt("Type rock, paper, or scissors");
-        console.log(play_round(player_choice.toLowerCase(), get_computer_choice()));
+
+function check_winner() {
+    if (player_score === 5 || computer_score === 5) {
+        const board = document.createElement('div');
+        container.appendChild(board);
+      const winner =
+        player_score === 5
+          ? "You win the game! Congratulations!"
+          : "Computer wins the game! Try again next time!";
+         board.textContent = winner;
+      return true;
     }
+    return false;
+
 }
-
-console.log(game());
-
-
-
-
-
-
